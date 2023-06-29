5aecf23a672da45c2aeba38e8bdb3e54fc0509b1
Irina <i.sus2011@yandex.ru>
2023-06-29 22:52:56 +0300

    feat: lj,fdbnm поддержку лайков и дизлайков

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,68 @@
 import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+ // const card = props.card;
+  // Определяем, являемся ли мы владельцем текущей карточки
+  //const isOwn = card.owner._id === currentUser._id;
+  const isOwn = props.card.owner._id === currentUser._id;
+
+  // Создаём переменную, которую после зададим в `className` для кнопки удаления
+  const cardDeleteButtonClassName = `element__button-delete ${
+    isOwn ? "hover" : ""
+  }`;
+
   const handleClick = () => {
     props.onCardClick(props.card);
   };
+
+  const handleLikeClick = () => {
+    props.onCardLike(props.card);
+  };
+
+  const handleDeleteClick = () => {
+    props.onCardDelete(props.card);
+  };
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+//Создаём переменную, которую после зададим в `className` для кнопки лайка
+const cardLikeButtonClassName = `element__button-like ${isLiked ? "element__button-like_activ" : ""}`;
 
   return (
     <article className="element" key={props.card._id}>
       <img
         src={props.card.link}
-      alt={`${props.title}.`}
+        alt={`${props.title}.`}
         className="element__photo"
         style={{ backgroundImage: `url(${props.src})` }}
         onClick={handleClick}
       />
+       {isOwn && (
       <button
         type="button"
-        className="element__button-delete hover"
+        className={cardDeleteButtonClassName}
         aria-label="удалить"
+        onClick={handleDeleteClick}
       ></button>
+      )}
+
       <h2 className="element__title">{props.title}</h2>
       <div className="element__likes">
         <button
           type="button"
-          className="element__button-like"
+          className={cardLikeButtonClassName}
           aria-label="нравится"
+          onClick={handleLikeClick}
         ></button>
-        <span className="element__number-like">{props.like.length}</span>
+       <span className="element__number-like">{props.like.length}</span>
+       {/* <span className="element__number-like">{props.card.likes.length}</span> */}
       </div>
     </article>
   );
 }
 
 export default Card;
+
