ce52e4bf29dd868ea12a460a7fb7bab311becb4d
Irina <i.sus2011@yandex.ru>
2023-06-30 20:36:38 +0300

    refactor:Создание компонента Аватар

---
+++ src/components/EditAvatarPopup.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PopupWithForm from "./PopupWithForm";
+
+function EditAvatarPopup(props) {
+  //записываем объект, возвращаемый хуком, в переменную
+  const EditAvatarPopupRef = React.useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    props.onUpdateAvatar({
+      avatar: EditAvatarPopupRef.current.value,
+    });
+  }
+
+  return (
+    <PopupWithForm
+      title="Обновить аватар"
+      name="avatar"
+      buttonText={"Сохранить"}
+      isOpen={props.isOpen}
+      onClose={props.onClose}
+      onSubmit={handleSubmit}
+    >
+      <input
+        ref={EditAvatarPopupRef}
+        type="url"
+        name="avatar"
+        className="popup__input"
+        id="avatar"
+        placeholder="Ссылка на картинку"
+        required
+      />
+      <span className="popup__error avatar-error"></span>
+    </PopupWithForm>
+  );
+}
+
+export default EditAvatarPopup;
